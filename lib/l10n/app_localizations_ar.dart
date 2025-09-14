ae6c2c83c2e385d6e1df8bc86e9beb8373675938
Abdelrhman0101 <abdalrhman.software.eg@gmail.com>
2025-09-14 18:07:30 +0300

    feat(i18n): add English and Arabic localization files
    build: update Android SDK versions and dependencies
    - Update minSdkVersion from 21 to 24
    - Update Android Gradle plugin to 8.6.0
    - Update Kotlin version to 1.9.10
    - Update intl package to ^0.20.2

---
+++ lib/l10n/app_localizations_ar.dart
@@ -0,0 +1,825 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get signUp => 'انشاء حساب';
+
+  @override
+  String get userName => 'اسم المستخدم';
+
+  @override
+  String get phone => 'رقم الهاتف';
+
+  @override
+  String get phoneNumberHint => '508236561';
+
+  @override
+  String get password => 'كلمة المرور';
+
+  @override
+  String get referralCode => 'كودالخصم';
+
+  @override
+  String get agreeTerms => 'أوافق على الشروط والأحكام';
+
+  @override
+  String get register => 'انشاء';
+
+  @override
+  String get or => 'أو';
+
+  @override
+  String get guestLogin => 'دخول كضيف';
+
+  @override
+  String get emailSignUp => 'تسجيل بالبريد الإلكتروني';
+
+  @override
+  String get login => 'تسجيل دخول';
+
+  @override
+  String get haveAccount => 'هل لديك حساب بالفعل؟';
+
+  @override
+  String get forgotPassword => 'هل نسيت كلمة المرور؟';
+
+  @override
+  String get emailLogin => 'الدخول بالبريد';
+
+  @override
+  String get dontHaveAccount => 'ليس لديك حساب؟';
+
+  @override
+  String get createAccount => 'إنشاء حساب';
+
+  @override
+  String get phonesignup => 'دخول برقم الهاتف';
+
+  @override
+  String get english => 'عربي';
+
+  @override
+  String get arabic => 'English';
+
+  @override
+  String get phoneLogin => 'دخول بالهاتف';
+
+  @override
+  String get myProfile => 'ملفي الشخصي';
+
+  @override
+  String get createAgentCode => 'إنشاء كود الخصم';
+
+  @override
+  String get notifications => 'الإشعارات';
+
+  @override
+  String get language => 'اللغة';
+
+  @override
+  String get contactUs => 'اتصل بنا';
+
+  @override
+  String get termsAndConditions => 'الشروط والأحكام';
+
+  @override
+  String get supportCenter => 'مركز الدعم';
+
+  @override
+  String get privacySecurity => 'الخصوصية والأمان';
+
+  @override
+  String get logout => 'تسجيل الخروج';
+
+  @override
+  String get whatsApp => 'واتساب';
+
+  @override
+  String get email => 'البريد الإلكتروني';
+
+  @override
+  String get advertiserName => 'اسم المُعلِن';
+
+  @override
+  String get advertiserType => 'نوع المُعلِن';
+
+  @override
+  String get advertiserLogo => 'شعار المُعلِن';
+
+  @override
+  String get uploadYourLogo => 'ارفع شعارك';
+
+  @override
+  String get advertiserLocation => 'موقع المُعلِن';
+
+  @override
+  String get address => 'العنوان';
+
+  @override
+  String get locateMe => 'حدد موقعي';
+
+  @override
+  String get save => 'حفظ';
+
+  @override
+  String get cancel => 'الغاء';
+
+  @override
+  String get optional => 'اختياري';
+
+  @override
+  String get premium => 'مميز';
+
+  @override
+  String get price => 'السعر';
+
+  @override
+  String get location => 'الموقع';
+
+  @override
+  String get placeName => 'منارة موتورز';
+
+  @override
+  String date(Object date) {
+    return 'Date: $date';
+  }
+
+  @override
+  String priceOnly(Object price) {
+    return '$price';
+  }
+
+  @override
+  String get back => 'رجوع';
+
+  @override
+  String get forgetyourpass => 'نسيت كلمة المرور؟';
+
+  @override
+  String get enterphone => 'ادخل رقم الهاتف';
+
+  @override
+  String get sendcode => 'ارسل الكود';
+
+  @override
+  String get enteremail => 'ادخل البريد الالكتروني';
+
+  @override
+  String get verifnum => 'تحقّق من رقمك';
+
+  @override
+  String get phoneverify =>
+      ' أرسلنا رسالة نصية تحتوي على رمز التفعيل إلى رقم هاتفك';
+
+  @override
+  String get emilverify =>
+      ' yourname@example.Com أرسلنا بريدًا إلكترونيًا يحتوي على رمز التفعيل إلى بريدك';
+
+  @override
+  String get verify => 'تحقّق';
+
+  @override
+  String get resetpass => 'إعادة تعيين كلمة المرور';
+
+  @override
+  String get newpass => 'كلمة المرور الجديدة';
+
+  @override
+  String get confirmpass => 'تأكيد كلمة المرور';
+
+  @override
+  String get confirm => 'تأكيد';
+
+  @override
+  String get favorites => 'المفضلة';
+
+  @override
+  String get carsales => 'بيع السيارات';
+
+  @override
+  String get realestate => 'العقارات';
+
+  @override
+  String get electronics => 'الإلكترونيات و \nالأجهزة المنزلية';
+
+  @override
+  String get jobs => 'وظائف';
+
+  @override
+  String get carrent => 'تأجير السيارات';
+
+  @override
+  String get carservices => 'خدمات السيارات';
+
+  @override
+  String get restaurants => 'مطاعم';
+
+  @override
+  String get otherservices => 'خدمات أخرى';
+
+  @override
+  String get home => 'الرئيسية';
+
+  @override
+  String get srtting => 'الإعدادات';
+
+  @override
+  String get manage => 'الإدارة';
+
+  @override
+  String get post => ' مكان إضافةإعلان';
+
+  @override
+  String get editprof4 => 'تعديل الملف الشخصي';
+
+  @override
+  String get editing1 => 'جاري التعديل';
+
+  @override
+  String get editit2 => 'هل تريد تعديل ملفك الشخصي؟';
+
+  @override
+  String get edit3 => 'تعديل';
+
+  @override
+  String get discover_best_cars_deals => 'اكتشف أفضل عروض السيارات';
+
+  @override
+  String get choose_make => 'اختر الماركة';
+
+  @override
+  String get choose_model => 'اختر الطراز';
+
+  @override
+  String get search => 'بحث';
+
+  @override
+  String get click_for_amazing_daily_cars_deals =>
+      'اضغط لرؤية عروض السيارات اليومية المميزة';
+
+  @override
+  String get top_premium_dealers => 'أفضل الوكلاء المميزين';
+
+  @override
+  String get see_all_ads => 'عرض كل الإعلانات';
+
+  @override
+  String get smart_search => 'بحث ذكي';
+
+  @override
+  String get km => 'كم';
+
+  @override
+  String get year => 'السنة';
+
+  @override
+  String get trim => 'الفئة';
+
+  @override
+  String get sort => 'فرز حسب الأقرب';
+
+  @override
+  String get ad => 'عدد الإعلانات';
+
+  @override
+  String get car_details => 'تفاصيل السيارة';
+
+  @override
+  String get car_type => 'نوع السيارة';
+
+  @override
+  String get trans_type => 'نوع ناقل الحركة';
+
+  @override
+  String get color => 'اللون';
+
+  @override
+  String get interior_color => 'اللون الداخلي';
+
+  @override
+  String get fuel_type => 'نوع الوقود';
+
+  @override
+  String get warranty => 'الضمان';
+
+  @override
+  String get doors_no => 'عدد الأبواب';
+
+  @override
+  String get seats_no => 'عدد المقاعد';
+
+  @override
+  String get engine_capacity => 'سعة المحرك';
+
+  @override
+  String get cylinders => 'عدد الأسطوانات';
+
+  @override
+  String get horse_power => 'قوة المحرك (حصان)';
+
+  @override
+  String get steering_side => 'جهة المقود';
+
+  @override
+  String get description => 'الوصف';
+
+  @override
+  String get agent => 'الوكيل';
+
+  @override
+  String get view_all_ads => 'عرض جميع الإعلانات';
+
+  @override
+  String get report_this_ad => 'الإبلاغ عن هذا الإعلان';
+
+  @override
+  String get use_this_space_for_ads =>
+      'تواصل معنا لاستخدام هذه المساحة لإعلاناتك';
+
+  @override
+  String get priority_first_premium => '🌟 إعلانات مميزة جدًا';
+
+  @override
+  String get priority_premium => '💎 إعلانات Premium';
+
+  @override
+  String get priority_featured => '🚀 إعلانات Featured';
+
+  @override
+  String get priority_free => '📢 إعلانات مجانية';
+
+  @override
+  String get add_to_favorite => 'إضافة إلى المفضلة';
+
+  @override
+  String get confirm_add_to_favorite => 'هل تريد إضافة هذا العنصر إلى المفضلة؟';
+
+  @override
+  String get yes => 'نعم';
+
+  @override
+  String get added_to_favorite => 'تمت إضافة العنصر إلى المفضلة';
+
+  @override
+  String get remove_from_favorite => 'حذف من المفضلة؟';
+
+  @override
+  String get confirm_remove_from_favorite => 'تأكيد الحذف من المفضلة';
+
+  @override
+  String get invisibleInfo =>
+      'عند تفعيل هذا الخيار، لن يتمكن الآخرون من معرفة أنك شاهدت إعلانهم أو تفاعلت معه.';
+
+  @override
+  String get invisibleTitle => 'التصفح المخفي';
+
+  @override
+  String get invisibleInfoTitle => 'ما هو وضع التخفي؟';
+
+  @override
+  String get ok => 'تمام';
+
+  @override
+  String get discover_deals => 'اكتشف أفضل عروض الإيجار';
+
+  @override
+  String get click_for_deals => 'اضغط للحصول على عروض إيجار يومية رائعة';
+
+  @override
+  String get discover_real_estate => 'اكتشف أفضل عروض العقارات';
+
+  @override
+  String get emirate => 'إمارة';
+
+  @override
+  String get district => 'منطقة';
+
+  @override
+  String get property_type => 'نوع العقار';
+
+  @override
+  String get contract_type => 'نوع العقد';
+
+  @override
+  String get click_for_deals_real_estate => 'اضغط لاستعراض أفضل عروض العقارات';
+
+  @override
+  String get discover_elect => 'اكتشف أفضل عروض الإلكترونيات';
+
+  @override
+  String get section_type => 'نوع القسم';
+
+  @override
+  String get section => 'القسم';
+
+  @override
+  String get product => 'المنتج';
+
+  @override
+  String get electronics2 => 'الإلكترونيات و';
+
+  @override
+  String get electronics3 => 'الأجهزة المنزلية';
+
+  @override
+  String get category => 'الفئة';
+
+  @override
+  String get type => 'النوع';
+
+  @override
+  String get contract => 'العقد';
+
+  @override
+  String get service => 'الخدمة';
+
+  @override
+  String get click_for_deals_elect =>
+      'اضغط لاستعراض أفضل عروض الأجهزة الإلكترونية والأدوات المنزلية';
+
+  @override
+  String get category_type => 'نوع التصنيف';
+
+  @override
+  String get click_for_deals_job => 'اضغط لرؤية عروض الوظائف المميزة';
+
+  @override
+  String get discover_best_job => 'اكتشف أفضل عروض الوظائف';
+
+  @override
+  String get service_type => 'اسم الخدمة';
+
+  @override
+  String get click_for_deals_car_service =>
+      'اضغط لرؤية أفضل عروض خدمات السيارات';
+
+  @override
+  String get discover_car_service => 'اكتشف أفضل عروض خدمات السيارات';
+
+  @override
+  String get district_choose => 'اختر المنطقة';
+
+  @override
+  String get click_daily_offers => 'اضغط لرؤية العروض اليومية';
+
+  @override
+  String get discover_restaurants_offers => 'اكتشف أفضل عروض المطاعم';
+
+  @override
+  String get click_daily_servir_offers => 'اضغط لرؤية عروض الخدمات المميزة';
+
+  @override
+  String get discover_service_offers => 'اكتشف أفضل عروض الخدمات';
+
+  @override
+  String get place_an_ad => 'نشر';
+
+  @override
+  String get featured => 'مُفضل';
+
+  @override
+  String get free => 'مجاني';
+
+  @override
+  String get appearance_top => 'الظهور في أعلى نتائج البحث';
+
+  @override
+  String get appearance_nearest => 'الظهور للمستخدمين الأقرب';
+
+  @override
+  String get appearance_after_star => 'الظهور بعد إعلان مميز ⭐';
+
+  @override
+  String get appearance_after_premium => 'الظهور بعد الإعلانات المميزة';
+
+  @override
+  String get appearance_after_featured => 'الظهور بعد الإعلانات المُفضلة';
+
+  @override
+  String get daily_refresh => 'تحديث يومي مفعّل';
+
+  @override
+  String get cost => 'السعر';
+
+  @override
+  String for_days(Object days) {
+    return 'لمدة [$days] يومًا';
+  }
+
+  @override
+  String get submit => 'إرسال';
+
+  @override
+  String get top_of_day_note => 'يظهر إعلان اليوم أولًا في نتائج البحث';
+
+  @override
+  String get appTitle => 'إعلانات بيع السيارات';
+
+  @override
+  String get toggleLang => 'English';
+
+  @override
+  String get make => 'النوع (الماركة)';
+
+  @override
+  String get model => 'الموديل';
+
+  @override
+  String get specs => 'المواصفات';
+
+  @override
+  String get title => 'العنوان';
+
+  @override
+  String get carType => 'نوع السيارة';
+
+  @override
+  String get transType => 'نوع الجير';
+
+  @override
+  String get fuelType => 'نوع الوقود';
+
+  @override
+  String get interiorColor => 'اللون الداخلي';
+
+  @override
+  String get engineCapacity => 'سعة المحرك';
+
+  @override
+  String get horsePower => 'قوة الحصان';
+
+  @override
+  String get doorsNo => 'عدد الأبواب';
+
+  @override
+  String get seatsNo => 'عدد المقاعد';
+
+  @override
+  String get steeringSide => 'جهة المقود';
+
+  @override
+  String get add => 'إضافة';
+
+  @override
+  String get phoneNumber => 'رقم الهاتف';
+
+  @override
+  String get area => 'المنطقة';
+
+  @override
+  String get describeYourCar => 'صف سيارتك';
+
+  @override
+  String get translate => 'ترجمة';
+
+  @override
+  String get addMainImage => 'إضافة الصورة الرئيسية';
+
+  @override
+  String get add14Images => 'إضافة 14 صورة';
+
+  @override
+  String get next => 'التالي';
+
+  @override
+  String get carsServicesAds => 'إعلانات خدمات السيارات';
+
+  @override
+  String get serviceType => 'نوع الخدمة';
+
+  @override
+  String get serviceName => 'اسم الخدمة';
+
+  @override
+  String get add3Images => 'إضافة 3 صور';
+
+  @override
+  String get realEstateAds => 'إعلانات العقارات';
+
+  @override
+  String get contractType => 'نوع العقد';
+
+  @override
+  String get propertyType => 'نوع العقار';
+
+  @override
+  String get add9Images => 'إضافة 9 صور';
+
+  @override
+  String get electronicsAndHomeAppliancesAds =>
+      'إعلانات الإلكترونيات والأجهزة المنزلية';
+
+  @override
+  String get productName => 'اسم المنتج';
+
+  @override
+  String get sectionType => 'نوع القسم';
+
+  @override
+  String get add4Images => 'إضافة 4 صور';
+
+  @override
+  String get carsRentAds => 'إعلانات تأجير السيارات';
+
+  @override
+  String get dayRent => 'الإيجار اليومي';
+
+  @override
+  String get monthRent => 'الإيجار الشهري';
+
+  @override
+  String get add10Images => 'إضافة 10 صور';
+
+  @override
+  String get restaurantsAds => 'إعلانات المطاعم';
+
+  @override
+  String get otherServicesAds => 'إعلانات الخدمات الأخرى';
+
+  @override
+  String get jobsAds => 'إعلانات الوظائف';
+
+  @override
+  String get categoryType => 'نوع الفئة';
+
+  @override
+  String get jobName => 'اسم الوظيفة';
+
+  @override
+  String get salary => 'الراتب';
+
+  @override
+  String get valid => 'صالح';
+
+  @override
+  String get pending => 'قيد المراجعة';
+
+  @override
+  String get expired => 'منتهي';
+
+  @override
+  String get rejected => 'مرفوض';
+
+  @override
+  String get postDate => 'تاريخ النشر';
+
+  @override
+  String get expiresIn => 'ينتهي خلال';
+
+  @override
+  String get views => 'بحث و مشاهدات';
+
+  @override
+  String get refresh => 'تحديث';
+
+  @override
+  String get edit => 'تعديل';
+
+  @override
+  String get renew => 'تجديد';
+
+  @override
+  String get upgrade => 'ترقية';
+
+  @override
+  String get adsType => 'نوع الإعلان';
+
+  @override
+  String get totalAds => 'مجموع الإعلانات';
+
+  @override
+  String get manageAds => 'إدارة الإعلانات';
+
+  @override
+  String get all => 'الكل';
+
+  @override
+  String get balance => 'الرصيد';
+
+  @override
+  String get contractExpire => 'تاريخ انتهاء العقد (استخدم الإعلانات قبل)';
+
+  @override
+  String get amount => 'المبلغ';
+
+  @override
+  String get days => 'أيام';
+
+  @override
+  String get activeOffersBox => 'Active Offers Box';
+
+  @override
+  String get pay => 'دفع';
+
+  @override
+  String get payment => 'الدفع';
+
+  @override
+  String get total => 'المجموع';
+
+  @override
+  String get payWithCreditCard => 'الدفع بالبطاقة الإئتمانية';
+
+  @override
+  String get cardNumber => 'رقم البطاقة';
+
+  @override
+  String get expireDate => 'تاريخ الإنتهاء';
+
+  @override
+  String get cvv => 'CVV';
+
+  @override
+  String get cardHolderName => 'اسم حامل البطاقة';
+
+  @override
+  String get payNow => 'ادفع الآن';
+
+  @override
+  String get noResultsFound => 'لا توجد نتائج';
+
+  @override
+  String get addNew => 'Add New';
+
+  @override
+  String get to => 'الى';
+
+  @override
+  String get any => 'أي';
+
+  @override
+  String get apply => 'تطبيق';
+
+  @override
+  String get from => 'من';
+
+  @override
+  String get reset => 'إعادة تعيين';
+
+  @override
+  String get chooseAnOption => 'اختر خيارًا';
+
+  @override
+  String get passwordTooShort => 'كلمة المرور قصيرة جدًا';
+
+  @override
+  String get passwordsDoNotMatch => 'كلمتا المرور غير متطابقتين';
+
+  @override
+  String get accountCreatedSuccessfully => 'تم إنشاء الحساب بنجاح';
+
+  @override
+  String get agreeTermsValidation => 'الموافقة على شروط التحقق';
+
+  @override
+  String get unknownError => 'خطأ غير معروف';
+
+  @override
+  String get pleaseEnterValidEmail => 'الرجاء إدخال بريد إلكتروني صحيح';
+
+  @override
+  String get pleaseEnterPhone => 'الرجاء إدخال رقم الهاتف';
+
+  @override
+  String get pleaseEnterUsername => 'الرجاء إدخال اسم المستخدم';
+
+  @override
+  String get searchCountry => 'البحث عن دولة';
+
+  @override
+  String get pleaseConfirmPassword => 'الرجاء تأكيد كلمة المرور';
+
+  @override
+  String get no => 'لا';
+
+  @override
+  String get read_more => 'قراءة المزيد';
+
+  @override
+  String get show_less => 'إظهار أقل';
+
+  @override
+  String get loading => 'جار التحميل';
+
+  @override
+  String get delete => 'حذف';
+
+  @override
+  String get please_select_make => 'الرجاء اختيار الماركة';
+
+  @override
+  String get please_fill_required_fields => 'الرجاء ملء الحقول المطلوبة';
+
+  @override
+  String get searchForLocation => 'البحث عن موقع';
+
+  @override
+  String get confirmLocation => 'تأكيد الموقع';
+}
